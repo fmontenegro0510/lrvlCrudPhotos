a52dd28414c6dbda04e2ebf31302748ff8d41483
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-07-19 18:06:26 -0300

    rutas agregardas y controllers creados

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,34 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Rutas autenticadas
+Route::middleware('auth')->group(function () {
+    // Rutas para los postulantes
+    Route::resource('postulantes', PostulanteController::class);
+
+    // Rutas para los concursos de un postulante
+    Route::get('postulantes/{postulante}/concursos/create', [ConcursoController::class, 'create'])->name('concursos.create');
+    Route::post('postulantes/{postulante}/concursos', [ConcursoController::class, 'store'])->name('concursos.store');
+    Route::get('postulantes/{postulante}/concursos/{concurso}/edit', [ConcursoController::class, 'edit'])->name('concursos.edit');
+    Route::put('postulantes/{postulante}/concursos/{concurso}', [ConcursoController::class, 'update'])->name('concursos.update');
+    Route::delete('postulantes/{postulante}/concursos/{concurso}', [ConcursoController::class, 'destroy'])->name('concursos.destroy');
+});
+
+// Rutas para la autenticación y registro de usuarios
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return view('auth.login');
+    })->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('/register', function () {
+        return view('auth.register');
+    })->name('register');
+
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+});
+
+// Ruta para cerrar sesión
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
